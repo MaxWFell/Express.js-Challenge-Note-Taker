088326483665c52e4bc91c44bc9264e739215ff0
Max Fell <Maxwilliamfell@gmail.com>
2023-02-14 18:45:39 -0500

    added consts

--- server.js
+++ server.js
@@ -2,18 +2,3 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Tells Express to create files.
-app.use(express.static('public'));
-// Gets express ready to take in data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//Routes to my route files!
-require('./routes/api')(app);
-require('./routes/html')(app);
-
-//Initalizes the server
-app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
-});
-Footer
